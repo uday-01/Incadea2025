306efa707f85e76d12efecd80285a41784d2a585
Ramya R <ramyawork22@gmail.com>
2025-02-18 21:57:12 +0530

    Added  new assignment

---
+++ ShoppingWebApi/EFCore/EF_DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingWebApi.EFCore
+{
+    public class EF_DataContext : DbContext
+    {
+        public EF_DataContext(DbContextOptions<EF_DataContext> options): base(options) 
+        {
+
+        }
+       
+        public DbSet<Product> Products { get; set; } 
+        public DbSet<Order> Orders { get; set; }
+    }
+}
